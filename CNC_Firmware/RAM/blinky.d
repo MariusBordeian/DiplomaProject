7c64c7ed4c3a3bf9500d29ae5877d45b5f477c52
JumpMan <m.bordeian@gmail.com>
2015-11-08 14:31:05 +0200

    Head Start
    CNC Firmware w/ GCode Python Tools and some Sample SVGs

---
+++ CNC_Firmware/RAM/blinky.d
@@ -0,0 +1,10 @@
+.\ram\blinky.o: Blinky.c
+.\ram\blinky.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\ram\blinky.o: C:\Keil_v5\ARM\Inc\ST\STM32F4xx\stm32f4xx.h
+.\ram\blinky.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm4.h
+.\ram\blinky.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\ram\blinky.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
+.\ram\blinky.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
+.\ram\blinky.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm4_simd.h
+.\ram\blinky.o: C:\Keil_v5\ARM\Inc\ST\STM32F4xx\system_stm32f4xx.h
+.\ram\blinky.o: LED.h
